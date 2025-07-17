4d5c19058722f3850cf9853755048ed2d2d4882a
ofctheuncleg <gferrarii19@gmail.com>
2025-07-17 20:05:25 +0100

    Create index.js
    Index

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>The Uncle Game - Tá no ar, Mr. Ferrari!</h1>
+      <p>🔥 Jogo de lógica, respeito e julgamentos. Seja bem-vindo.</p>
+    </div>
+  );
+}
